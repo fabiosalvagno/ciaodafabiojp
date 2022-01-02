ca35ced60e6da8ef82dfdb64bee595092ddf6481
Fabio Salvagno <fabiosalvagno@yahoo.co.jp>
2022-01-03 08:53:06 +0900

    primo commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Sawarabi+Mincho" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <title>Ciao da Fabio</title>
+</head>
+<body>
+<section class="nav">
+    <div class="navbar">
+        <div class="title">
+            Ciao da Fabio
+        </div>
+        <div class="menu wf-sawarabimincho">
+            <ul>
+                <a href=""><li>Home</li></a>
+                <a href=""><li>テキスト</li></a>
+                <a href=""><li>イタリア語講座</li></a>
+            </ul>
+        </div>
+    </div>
+</section>
+<section class="main">
+    <div class="container">
+        <p>prova</p>
+    </div>
+</section>
+</body>
+</html>
