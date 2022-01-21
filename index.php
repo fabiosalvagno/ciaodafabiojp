343ec74050196a8f06f1e737ed24ee15898361ba
Fabio Salvagno <fabiosalvagno@yahoo.co.jp>
2022-01-21 11:22:27 +0900

    changed the index file

--- index.php
+++ index.php
@@ -1,34 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Sawarabi+Mincho&display=swap" rel="stylesheet"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sawarabi+Mincho&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style.css">
     <title>Ciao da Fabio</title>
 </head>
+
 <body>
-<section class="nav">
-    <div class="navbar">
-        <div class="title">
-            Ciao da Fabio
+    <section class="nav">
+        <div class="navbar">
+            <div class="title">
+                Ciao da Fabio 2
+            </div>
+            <div class="menu wf-sawarabimincho">
+                <ul>
+                    <a href="">
+                        <li>Home</li>
+                    </a>
+                    <a href="">
+                        <li>テキスト</li>
+                    </a>
+                    <a href="">
+                        <li>イタリア語講座</li>
+                    </a>
+                </ul>
+            </div>
         </div>
-        <div class="menu wf-sawarabimincho">
-            <ul>
-                <a href=""><li>Home</li></a>
-                <a href=""><li>テキスト</li></a>
-                <a href=""><li>イタリア語講座</li></a>
-            </ul>
+    </section>
+    <section class="main">
+        <div class="container">
+            <p class="center">サイトを作成中28</p>
         </div>
-    </div>
-</section>
-<section class="main">
-    <div class="container">
-        <p class="center">サイトを作成中28</p>
-    </div>
-</section>
+    </section>
 </body>
+
 </html>
